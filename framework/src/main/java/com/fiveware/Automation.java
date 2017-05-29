c81cb0e07792778406d88011a77e1952eb2e18e7
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-05-29 11:38:57 -0300

    Alteracao interface Automation generics extends Serializable

--- framework/src/main/java/com/fiveware/Automation.java
+++ framework/src/main/java/com/fiveware/Automation.java
@@ -1,11 +1,13 @@
 package com.fiveware;
 
+import java.io.Serializable;
+
 import com.fiveware.model.OutTextRecord;
 
 /**
  * Created by valdisnei on 24/05/17.
  */
-public interface Automation<T> {
+public interface Automation<T extends Serializable> {
     	
 	OutTextRecord execute(T recordLine);
 }
