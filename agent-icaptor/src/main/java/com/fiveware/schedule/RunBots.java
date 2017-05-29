c656182bbffdfe68cdb3a670828e92a5f9fcafe6
Valdisnei <valdisnei@MacBook-Pro-de-Valdisnei.local>
2017-05-28 23:00:03 -0300

    other test project

--- agent-icaptor/src/main/java/com/fiveware/schedule/RunBots.java
+++ agent-icaptor/src/main/java/com/fiveware/schedule/RunBots.java
@@ -1,5 +1,6 @@
 package com.fiveware.schedule;
 
+import com.fiveware.converter.ConverterRecordLine;
 import com.fiveware.util.PropertiesCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,13 +11,13 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.*;
-import java.util.Enumeration;
+import java.util.Properties;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -33,54 +34,31 @@ public class RunBots {
     @Value("${loader.path}")
     private String directory;
 
-
-    @Autowired
-    private PropertiesCache propertiesCache;
-
-
     @Scheduled(fixedRate = 5000)
-    public void run() throws IOException {
+    public void run() throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         File file = new File(directory);
+        JarFile jarFile = new JarFile(file);
+
+//        jarFile.stream().forEach( jarEntry -> System.out.println("jarEntry = " + jarEntry.getName()) );
+
+        JarEntry jarEntry = jarFile.getJarEntry("application.properties");
+        JarEntry fileEntry = jarFile.getJarEntry(jarEntry.getName());
+        InputStream input = jarFile.getInputStream(fileEntry);
+        Properties configProp = new Properties();
+        configProp.load(input);
+        String className2 = configProp.getProperty("icaptor.class.main");
+        String method = configProp.getProperty("icaptor.method.execute");
+
 
         ClassLoader classLoader = new URLClassLoader(new URL[]{file.toURI().toURL()});
-        Enumeration<URL> en = classLoader.getResources("META-INF");
+        Class cls = classLoader.loadClass(className2);
+        Object o = cls.newInstance();
+        Method execute = cls.getMethod(method, String.class);
+        Object obj= execute.invoke(o, "07077170");
 
-        if (en.hasMoreElements()) {
-			if (en.hasMoreElements()) {
-				URL url2 = en.nextElement();
-				JarURLConnection urlcon = (JarURLConnection) (url2.openConnection());
-				try (JarFile jar = urlcon.getJarFile();) {
-					Enumeration<JarEntry> entries = jar.entries();
-					while (entries.hasMoreElements()) {
-						String entry = entries.nextElement().getName();
-						System.out.println(entry);
-                        if ("application.properties".equals(entry))
-                            openProperties(classLoader, entry);
-					}
-				}
-			}
-        }
-    }
-
-    private void openProperties(ClassLoader classLoader, String entry) {
-        propertiesCache.load(classLoader, entry);
-        String className = propertiesCache.getProperty("icaptor.class.main");
-        String method = propertiesCache.getProperty("icaptor.method.execute");
-
-        Class cls = null;
-        try {
-            cls = classLoader.loadClass(className);
-            Object o = cls.newInstance();
-            Method execute = cls.getMethod(method, Object.class);
-            execute.invoke(o, "07077170");
-
-        } catch (ClassNotFoundException | IllegalAccessException | InvocationTargetException
-                | NoSuchMethodException | InstantiationException e) {
-            logger.error("error {}", e);
-        }
+        System.out.println(obj);
 
     }
-
 
     public void wacthDirectory() throws IOException {
         final Path path = FileSystems.getDefault().getPath(System.getProperty(directory));
--- correios/src/main/java/com/fiveware/CorreioApplication.java
+++ correios/src/main/java/com/fiveware/CorreioApplication.java
@@ -6,8 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class CorreioApplication implements CommandLineRunner {
 
 	static Logger logger = LoggerFactory.getLogger(CorreioApplication.class);
