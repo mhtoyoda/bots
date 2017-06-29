3bb9ba116f181e62691815d43c0138e587826bb9
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-06-29 13:17:50 -0300

    Refactoring Bot teste cep com api dsl automate

--- teste/src/main/java/com/fiveware/TesteBot.java
+++ teste/src/main/java/com/fiveware/TesteBot.java
@@ -1,15 +1,20 @@
 package com.fiveware;
 
-import com.fiveware.annotation.*;
-import com.fiveware.automate.BotJS;
-import com.fiveware.automate.BotWebBrowser;
-import com.fiveware.automate.BotWebDriver;
-import com.fiveware.exception.ExceptionBot;
-import org.openqa.selenium.By;
+import static com.fiveware.automate.BotAutomationBuilder.Web;
+
+import java.util.Iterator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Iterator;
+import com.fiveware.annotation.Field;
+import com.fiveware.annotation.Icaptor;
+import com.fiveware.annotation.IcaptorMethod;
+import com.fiveware.annotation.InputDictionary;
+import com.fiveware.annotation.OutputDictionary;
+import com.fiveware.automate.BotScreen;
+import static com.fiveware.automate.BotWebBrowser.*;
+import com.fiveware.exception.ExceptionBot;
 
 /**
  * Created by valdisnei on 5/28/17.
@@ -36,45 +41,37 @@ public class TesteBot implements Automation<String, Endereco> {
 	}
 
 	public Endereco getEndereco(String args) {
-		BotWebDriver botWebDriver = new BotWebDriver();
-		botWebDriver.initialize(BotWebBrowser.PHANTOM);
-		
 		String baseUrl = "http://www.correios.com.br/";
+		BotScreen telaConsultaCep = Web().driver(PHANTOM).openPage(baseUrl + "/para-voce");
+		telaConsultaCep.windowMaximize();
+		telaConsultaCep.find().elementBy().id("acesso-busca").sendKeys(args);		
+		telaConsultaCep.find().elementBy().cssSelector("input.acesso-busca-submit").click();
+		
+		telaConsultaCep.waitForPageToLoadFor(400);
 
-		botWebDriver.openPageBrowser(baseUrl + "/para-voce");		
-		botWebDriver.manage().window().maximize();
-		botWebDriver.findElement(By.id("acesso-busca")).clear();
-		botWebDriver.findElement(By.id("acesso-busca")).sendKeys(args);
-		botWebDriver.findElement(By.cssSelector("input.acesso-busca-submit")).click();
-
-		new BotJS(botWebDriver).waitForPageToBeReady();
-
-		Iterator<String> windows = botWebDriver.getWindowHandles().iterator();
+		Iterator<String> windows = telaConsultaCep.iteratorWindowHandles();
 		windows.next();
 
-		botWebDriver.switchTo().window(windows.next());
+		telaConsultaCep.window(windows.next());
 
-		String resultado = botWebDriver
-				.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/p")).getText();
+		String resultado = telaConsultaCep.find()
+				.elementBy().xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/p").getText();
 
-		if ("DADOS NAO ENCONTRADOS".equalsIgnoreCase(resultado))
-			return null;
+		if ("DADOS NAO ENCONTRADOS".equalsIgnoreCase(resultado)){
+			return null;			
+		}
 
-		String logradouro = botWebDriver
-				.findElement(By
-						.xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/table/tbody/tr[2]/td[1]"))
+		String logradouro = telaConsultaCep.find()
+				.elementBy().xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/table/tbody/tr[2]/td[1]")
 				.getText();
-		String bairro = botWebDriver
-				.findElement(By
-						.xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/table/tbody/tr[2]/td[2]"))
+		String bairro = telaConsultaCep.find()
+				.elementBy().xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/table/tbody/tr[2]/td[2]")
 				.getText();
-		String localidade = botWebDriver
-				.findElement(By
-						.xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/table/tbody/tr[2]/td[3]"))
+		String localidade = telaConsultaCep.find()
+				.elementBy().xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/table/tbody/tr[2]/td[3]")
 				.getText();
-		String cep = botWebDriver
-				.findElement(By
-						.xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/table/tbody/tr[2]/td[4]"))
+		String cep = telaConsultaCep.find()
+				.elementBy().xpath("/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/table/tbody/tr[2]/td[4]")
 				.getText();
 
 		logger.info(" Endereco: {} - {} - {} - {}", logradouro, bairro, localidade, cep);
