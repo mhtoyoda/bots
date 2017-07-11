64a47042c95084218a47c158578aeefda2bf9dbd
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-07-11 15:10:19 -0300

    Script de versionamento

---
+++ icaptor-core/src/main/resources/db/migration/V7__new_table_parameter_bot.sql
@@ -0,0 +1,9 @@
+CREATE TABLE parameter_bot (
+  id BIGINT(20) NOT NULL,
+  parameter VARCHAR(60) NOT NULL,
+  value VARCHAR(180) NOT NULL,
+  id_bot BIGINT(20) NOT NULL,
+  ativo BOOLEAN DEFAULT true,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_bot) REFERENCES bot(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
