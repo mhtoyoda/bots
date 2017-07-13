b52436b22035989e7d4bd448214111763ff685b4
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-07-13 14:19:56 -0300

    Script de criacao de tabela para armazenar tasks

---
+++ icaptor-cloud/src/main/resources/db/migration/V9__new_table_task.sql
@@ -0,0 +1,10 @@
+CREATE TABLE task (
+  id BIGINT(20) NOT NULL,
+  create_time	DATETIME NOT NULL,
+  last_update_time	DATETIME NOT NULL,
+  start_time	DATETIME,
+  end_time	    DATETIME,
+  count_executions BIGINT(20) NULL,
+  status VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
