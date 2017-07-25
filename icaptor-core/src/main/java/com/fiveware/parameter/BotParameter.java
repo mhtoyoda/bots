0d3a3bb9f020b89cd2740274222d767f9f8c982b
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-07-25 01:38:07 -0300

    Imports

--- icaptor-core/src/main/java/com/fiveware/parameter/BotParameter.java
+++ icaptor-core/src/main/java/com/fiveware/parameter/BotParameter.java
@@ -1,38 +1,37 @@
 package com.fiveware.parameter;
 
-import com.fiveware.model.BotParameterKeyValue;
-import com.fiveware.model.ParameterValueBot;
-import com.fiveware.service.ServiceParameterBotValue;
-import com.google.common.collect.Lists;
-import org.apache.commons.collections4.CollectionUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.fiveware.model.BotParameterKeyValue;
+import com.fiveware.model.ParameterValueBot;
+import com.fiveware.service.ServiceParameterBotValue;
+import com.google.common.collect.Lists;
 
 @Component
 public class BotParameter {
-	
+
 	@Autowired
 	private ServiceParameterBotValue serviceParameterBotValue;
 
-	
-	public List<Map<String, BotParameterKeyValue>> loadParameterBot(String nameBot){
+	public List<Map<String, BotParameterKeyValue>> loadParameterBot(String nameBot) {
 		List<Map<String, BotParameterKeyValue>> parameters = Lists.newArrayList();
 		List<ParameterValueBot> list = serviceParameterBotValue.findByParameterBotValues(nameBot);
-		if(CollectionUtils.isNotEmpty(list)){
+		if (CollectionUtils.isNotEmpty(list)) {
 			Map<String, BotParameterKeyValue> map = new LinkedHashMap<String, BotParameterKeyValue>();
 			list.forEach(param -> {
 				String key = param.getParameterBot().getName();
-				if(map.containsKey(key)){
+				if (map.containsKey(key)) {
 					map.get(key).add(param.getParameter(), param.getValue());
-				}else{
-					map.put(key ,new BotParameterKeyValue().add(param.getParameter(), param.getValue()));					
+				} else {
+					map.put(key, new BotParameterKeyValue().add(param.getParameter(), param.getValue()));
 				}
-			});			
+			});
 			parameters.add(map);
 		}
 		return parameters;
