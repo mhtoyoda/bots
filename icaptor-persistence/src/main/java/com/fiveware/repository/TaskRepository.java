ce858cbf626f677e48d11adc31baf70f5a63957e
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-08-04 14:57:39 -0300

    Tabelas de Tasks

--- icaptor-persistence/src/main/java/com/fiveware/repository/TaskRepository.java
+++ icaptor-persistence/src/main/java/com/fiveware/repository/TaskRepository.java
@@ -1,18 +1,11 @@
 package com.fiveware.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fiveware.model.Task;
 
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long>{
-
-	@Query("FROM Task t WHERE t.bot.nameBot = :nameBot AND t.status = :status")
-	List<Task> getAllTaskBotByStatus(@Param("nameBot") String nameBot, @Param("status") String status);
 
 }
