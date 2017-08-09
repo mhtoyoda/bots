debfe7960dbbb21fd9814e0cea12129bdf4023b9
valdisnei <valdisnei.fajardo@gmail.com>
2017-08-09 12:34:43 -0300

    #ICAPTOR-109 - refactoring platform

--- icaptor-processing/src/main/java/com/fiveware/util/LineUtil.java
+++ icaptor-processing/src/main/java/com/fiveware/util/LineUtil.java
@@ -1,37 +1,12 @@
 package com.fiveware.util;
 
-import java.io.IOException;
-import java.util.List;
-
+import com.fiveware.model.Record;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
-
-import com.fiveware.model.Record;
-import com.google.common.collect.Lists;
 
 @Component
 public class LineUtil {
 
-	public List<Record> linesFrom(List<String> linhas, String[] fields, String separator) throws IOException {
-		List<Record> lines = getLines(linhas, fields, separator);
-		return lines;
-	}
-
-	public List<Record> getLines(List<String> lines, String[] fields, String separator) {
-		List<Record> recordsLines = Lists.newArrayList();
-
-		lines.forEach(record -> {
-			String[] recordArray = StringUtils.split(record, separator);
-			if (recordArray.length == fields.length) {
-				Record recordLines = new Record();
-				for (int i = 0; i < fields.length; i++) {
-					recordLines.addRecordLine(fields[i], recordArray[i]);
-				}
-				recordsLines.add(recordLines);
-			}
-		});
-		return recordsLines;
-	}
 
 	public Record linesFrom(String line, String[] fields, String separator) {
 		Record record = new Record();
