2712423c6530c0a8e10d2875da94459a9b461520
diego.souza <diego.souza@fiveware.com.br>
2017-08-10 18:05:58 -0300

    Inicio da criacao do controller

--- icaptor-cloud/src/main/java/com/fiveware/web/controller/ControlPanelController.java
+++ icaptor-cloud/src/main/java/com/fiveware/web/controller/ControlPanelController.java
@@ -3,6 +3,8 @@ package com.fiveware.web.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControlPanelController {
 
 	private static final Logger logger = LoggerFactory.getLogger(ControlPanelController.class);
-	
+
 	@GetMapping("/loadUserInfo")
-	public ResponseEntity<Object> loadUserInfo() {
-		logger.info("Carregando as informacoes do usuario.");
+	public ResponseEntity<Object> loadUserInfo(@AuthenticationPrincipal User user) {
+		logger.info("Carregando as informacoes do usuario [{}]", user.getUsername());
+
 		return ResponseEntity.ok().build();
 	}
 
@@ -29,8 +32,21 @@ public class ControlPanelController {
 	public ResponseEntity<Object> loadRecentActivities() {
 		return ResponseEntity.ok().build();
 	}
+
 	@GetMapping("/loadFilterOptions")
 	public ResponseEntity<Object> loadFilterOptions() {
+		return ResponseEntity.ok().build();
+	}
+
+	public ResponseEntity<Object> pauseTask() {
+		return ResponseEntity.ok().build();
+	}
+
+	public ResponseEntity<Object> resumeTask() {
+		return ResponseEntity.ok().build();
+	}
+
+	public ResponseEntity<Object> cancelTask() {
 		return ResponseEntity.ok().build();
 	}
 }
