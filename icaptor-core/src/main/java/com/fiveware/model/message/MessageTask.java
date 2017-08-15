77d818223e54054cff538cde4818339ecbb516cb
valdisnei <valdisnei.fajardo@gmail.com>
2017-08-15 12:49:52 -0300

    #ICAPTOR-109 - tratamento de Erros

--- icaptor-core/src/main/java/com/fiveware/model/message/MessageTask.java
+++ icaptor-core/src/main/java/com/fiveware/model/message/MessageTask.java
@@ -1,29 +1,18 @@
 package com.fiveware.model.message;
 
-<<<<<<< HEAD
 import com.fiveware.messaging.TypeMessage;
 import org.pojomatic.Pojomatic;
 import org.pojomatic.annotations.AutoProperty;
 
 import java.io.Serializable;
 
-=======
-import java.io.Serializable;
-
-import org.pojomatic.Pojomatic;
-import org.pojomatic.annotations.AutoProperty;
-
->>>>>>> 564bcc835f6601382c022a18bfbff6a43fa008c7
 @AutoProperty
 public class MessageTask implements Serializable{
 
 	private String nameQueueTask;
 	private String botName;
-<<<<<<< HEAD
 	private TypeMessage typeMessage;
 
-=======
->>>>>>> 564bcc835f6601382c022a18bfbff6a43fa008c7
 
 	public MessageTask(String nameQueueTask, String botName) {
 		super();
@@ -38,7 +27,6 @@ public class MessageTask implements Serializable{
 	public String getBotName() {
 		return botName;
 	}
-<<<<<<< HEAD
 
 	public TypeMessage getTypeMessage() {
 		return typeMessage;
@@ -46,9 +34,6 @@ public class MessageTask implements Serializable{
 
 
 
-=======
-	
->>>>>>> 564bcc835f6601382c022a18bfbff6a43fa008c7
 	@Override
 	public String toString() {
 		return Pojomatic.toString(this);
