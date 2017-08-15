793f166a7aca5b1a9d4af8f990509123d490e7ef
diego.souza <diego.souza@fiveware.com.br>
2017-08-15 17:10:13 -0300

    Refactor das tabelas de usuario

--- icaptor-persistence/src/main/resources/db/migration/V5__new_table_usuarios.sql
+++ icaptor-persistence/src/main/resources/db/migration/V5__new_table_usuarios.sql
@@ -1,36 +1,65 @@
-CREATE TABLE usuario (
+CREATE TABLE company (
+	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE user (
   id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(50) NOT NULL,
+  display_name VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  senha VARCHAR(120) NOT NULL,
-  ativo BOOLEAN DEFAULT true
+  password VARCHAR(150) NOT NULL,
+  active BOOLEAN DEFAULT true,
+  profile_image_path VARCHAR(50)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE grupo (
-  id BIGINT(20) PRIMARY KEY,
-  nome VARCHAR(50) NOT NULL
+CREATE TABLE `group` (
+  id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE permissao (
-  id BIGINT(20) PRIMARY KEY,
-  nome VARCHAR(50) NOT NULL
+CREATE TABLE permission (
+  id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE usuario_grupo (
-  id_usuario BIGINT(20) NOT NULL,
-  id_grupo BIGINT(20) NOT NULL,
-  PRIMARY KEY (id_usuario, id_grupo),
-  FOREIGN KEY (id_usuario) REFERENCES usuario(id),
-  FOREIGN KEY (id_grupo) REFERENCES grupo(id)
+CREATE TABLE company_group (
+	id_company BIGINT(20) NOT NULL,
+	id_group BIGINT(20) NOT NULL,
+    PRIMARY KEY (id_company, id_group),
+	FOREIGN KEY (id_company) REFERENCES company(id),
+	FOREIGN KEY (id_group) REFERENCES `group`(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE group_user (
+  id_user BIGINT(20) NOT NULL,
+  id_group BIGINT(20) NOT NULL,
+  PRIMARY KEY (id_user, id_group),
+  FOREIGN KEY (id_user) REFERENCES user(id),
+  FOREIGN KEY (id_group) REFERENCES `group`(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE grupo_permissao (
-  id_grupo BIGINT(20) NOT NULL,
-  id_permissao BIGINT(20) NOT NULL,
-  PRIMARY KEY (id_grupo, id_permissao),
-  FOREIGN KEY (id_grupo) REFERENCES grupo(id),
-  FOREIGN KEY (id_permissao) REFERENCES permissao(id)
+CREATE TABLE group_permission (
+  id_group BIGINT(20) NOT NULL,
+  id_permission BIGINT(20) NOT NULL,
+  PRIMARY KEY (id_group, id_permission),
+  FOREIGN KEY (id_group) REFERENCES `group`(id),
+  FOREIGN KEY (id_permission) REFERENCES permission(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-insert into usuario (nome, email, senha, ativo)
-  VALUES ("admin","admin@icaptor.com",'$2a$10$0ympKfE9MW6ERWelQBvPNOO2GNI2AZ6isfRCayMUsb6k3rnrFX/Yq',1);
+INSERT INTO user (display_name, email, password, active) VALUES ("admin","admin@icaptor.com",'$2a$10$0ympKfE9MW6ERWelQBvPNOO2GNI2AZ6isfRCayMUsb6k3rnrFX/Yq',1);
+
+INSERT INTO company (name) VALUES ('Fiveware');
+
+INSERT INTO `group` (name) VALUES ('administrator');
+INSERT INTO `group` (name) VALUES ('developer');
+
+INSERT INTO permission (name) VALUES ('ALL');
+INSERT INTO permission (name) VALUES ('DEV');
+
+INSERT INTO company_group (id_company, id_group) VALUES (1,1);
+INSERT INTO company_group (id_company, id_group) VALUES (1,2);
+
+INSERT INTO group_user (id_user, id_group) VALUES (1,2);
+
+INSERT INTO group_permission (id_group, id_permission) VALUES (1,1);
+INSERT INTO group_permission (id_group, id_permission) VALUES (1,2);
