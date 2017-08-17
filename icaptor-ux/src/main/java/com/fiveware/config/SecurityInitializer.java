826c34571dac65df71efd6470f743c54ec836782
diego.souza <diego.souza@fiveware.com.br>
2017-08-16 22:04:47 -0300

    Removendo modulo icaptor-ux

--- icaptor-ux/src/main/java/com/fiveware/config/SecurityInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.fiveware.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * Created by valdisnei on 7/6/17.
- */
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
