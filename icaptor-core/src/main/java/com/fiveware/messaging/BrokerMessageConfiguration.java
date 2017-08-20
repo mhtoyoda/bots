8e52141e0d087a6c2c4dbe73cbad6b762b2b2ee2
valdisnei <valdisnei.fajardo@gmail.com>
2017-08-20 16:16:41 -0300

    #ICAPTOR-141 - implementaoca ACK and NACK

--- icaptor-core/src/main/java/com/fiveware/messaging/BrokerMessageConfiguration.java
+++ icaptor-core/src/main/java/com/fiveware/messaging/BrokerMessageConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -45,10 +47,29 @@ public class BrokerMessageConfiguration {
 	public Binding binding(Queue queue, TopicExchange exchange) {
 		return BindingBuilder.bind(queue).to(exchange).with(QueueName.EVENTS.name());
 	}
-	
+
 	@Bean
 	public RabbitAdmin rabbitAdmin(){
 		return new RabbitAdmin(connectionFactory());
+	}
+
+	@Bean
+	public RabbitTemplate rabbitTemplate(){
+//		return new RabbitAdmin(connectionFactory());
+
+		RabbitTemplate rabbitTemplate = new RabbitTemplate( connectionFactory() );
+		((CachingConnectionFactory)rabbitTemplate.getConnectionFactory()).setPublisherConfirms( true );
+		rabbitTemplate.setConfirmCallback( new RabbitTemplate.ConfirmCallback() {
+
+			@Override
+			public void confirm(CorrelationData corData, boolean ack, String cause ) {
+				System.out.println( "devconfig.rabbitTemplate(...).new ConfirmCallback() {...}.confirm()"+corData );
+				System.out.println( "devconfig.rabbitTemplate(...).new ConfirmCallback() {...}.confirm()"+ack );
+			}
+		} );
+
+		return rabbitTemplate;
+
 	}
 
 }
--- icaptor-core/src/main/java/com/fiveware/messaging/EventBotReceiver.java
+++ icaptor-core/src/main/java/com/fiveware/messaging/EventBotReceiver.java
@@ -1,20 +1,32 @@
 package com.fiveware.messaging;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.util.Objects;
-
+import com.fiveware.model.message.MessageBot;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.ShutdownSignalException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.SerializationUtils;
 
-import com.fiveware.model.message.MessageBot;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Service("eventBotReceiver")
-public class EventBotReceiver implements Receiver<MessageBot> {
-	
+public class EventBotReceiver implements Receiver<MessageBot>,Consumer {
+
+
+	static Logger logger = LoggerFactory.getLogger(EventBotReceiver.class);
+
+
 	@Autowired
 	private RabbitTemplate rabbit;
 	
@@ -33,5 +45,36 @@ public class EventBotReceiver implements Receiver<MessageBot> {
 		} catch (IOException | ClassNotFoundException e) {
 			return null;
 		}
+	}
+
+	@Override
+	public void handleConsumeOk(String s) {
+		logger.debug(s);
+	}
+
+	@Override
+	public void handleCancelOk(String s) {
+		logger.debug(s);
+	}
+
+	@Override
+	public void handleCancel(String s) throws IOException {
+		logger.debug(s);
+	}
+
+	@Override
+	public void handleShutdownSignal(String s, ShutdownSignalException e) {
+		logger.debug(s);
+	}
+
+	@Override
+	public void handleRecoverOk(String s) {
+		logger.debug(s);
+	}
+
+	@Override
+	public void handleDelivery(String s, Envelope envelope, AMQP.BasicProperties basicProperties, byte[] bytes) throws IOException {
+		Map map = (HashMap) SerializationUtils.deserialize(bytes);
+		System.out.println("Message Number "+ map.get("message number") + " received.");
 	}
 }
