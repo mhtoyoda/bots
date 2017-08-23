222da200bba61044f7a046f8b4e68740a2664fdb
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-08-23 11:25:40 -0300

    ICAPTOR-131 novas anotacoes de parametros para o BOT

---
+++ icaptor-framework/src/main/java/com/fiveware/annotation/IcaptorParameter.java
@@ -0,0 +1,24 @@
+package com.fiveware.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * Configura parametros referente a automação
+ * 
+ */
+@Target(value = {ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(value = IcaptorParameters.class)
+public @interface IcaptorParameter {
+	
+	String name();
+	String value();
+	String regexValidate() default "";
+	String nameTypeParameter();
+	boolean exclusive();
+}
