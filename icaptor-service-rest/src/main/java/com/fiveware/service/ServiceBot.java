df14b6126470eac1fdc74f767edf4d40b4d7e231
diego.souza <diego.souza@fiveware.com.br>
2017-08-31 13:34:12 -0300

    Controller para carregar os bots para a tela

--- icaptor-service-rest/src/main/java/com/fiveware/service/ServiceBot.java
+++ icaptor-service-rest/src/main/java/com/fiveware/service/ServiceBot.java
@@ -1,6 +1,8 @@
 package com.fiveware.service;
 
-import com.fiveware.model.Bot;
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,39 +13,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Optional;
+import com.fiveware.model.Bot;
 
-/**
- * Created by valdisnei on 29/05/17.
- */
 @Service
 public class ServiceBot {
 
-    static Logger logger = LoggerFactory.getLogger(ServiceBot.class);
+	private static Logger logger = LoggerFactory.getLogger(ServiceBot.class);
 
-    @Autowired
-    private RestTemplate restTemplate;
+	private static final String BASE_URL = "http://localhost:8085/api/bot";
 
+	@Autowired
+	private RestTemplate restTemplate;
 
-    public Optional<Bot> findByNameBot(String nameBot){
+	public Optional<Bot> findByNameBot(String nameBot) {
+		String url = BASE_URL + "/name/" + nameBot;
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
 
-        String url = "http://localhost:8085/api/bot/name/"+nameBot;
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+		Bot bot = restTemplate.getForObject(url, Bot.class);
+		return Optional.of(bot);
+	}
 
-        Bot bot = restTemplate.getForObject(url, Bot.class);
-        return Optional.of(bot);
-    }
+	public List<Bot> findAll() {
+		return restTemplate.getForObject(BASE_URL, List.class);
+	}
 
+	public Bot save(Bot bot) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
 
-    public Bot save(Bot bot){
-        String url = "http://localhost:8085/api/bot";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<Bot> entity = new HttpEntity<Bot>(bot,headers);
-        ResponseEntity<Bot> botResponseEntity = restTemplate.postForEntity(url, entity, Bot.class);
-        return botResponseEntity.getBody();
-    }
+		HttpEntity<Bot> entity = new HttpEntity<Bot>(bot, headers);
+		ResponseEntity<Bot> botResponseEntity = restTemplate.postForEntity(BASE_URL, entity, Bot.class);
+		return botResponseEntity.getBody();
+	}
 
 }
