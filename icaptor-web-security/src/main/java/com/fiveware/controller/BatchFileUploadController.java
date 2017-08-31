e3bd5ece2771633f1aa7e5cce2c6b0e4339cc271
diego.souza <diego.souza@fiveware.com.br>
2017-08-31 13:34:12 -0300

    Controller para carregar os bots para a tela

--- icaptor-web-security/src/main/java/com/fiveware/controller/BatchFileUploadController.java
+++ icaptor-web-security/src/main/java/com/fiveware/controller/BatchFileUploadController.java
@@ -1,28 +1,33 @@
 package com.fiveware.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fiveware.model.Bot;
+import com.fiveware.service.ServiceBot;
+
 @RestController
-@RequestMapping("/batchUpload")
+@RequestMapping("/file-upload")
 public class BatchFileUploadController {
 
 	private static final Logger logger = LoggerFactory.getLogger(BatchFileUploadController.class);
-	
-	@GetMapping("/loadAvailable")
-	public ResponseEntity<Object> loadAvailableBots(){
-		logger.info("Carregando a lista de bots disponiveis para esse usuario");
-		return ResponseEntity.ok().build();
-	}
-	
-	@GetMapping("/createTask")
-	public ResponseEntity<Object> createTask(){
-		logger.info("Criando uma nova task");
-		return ResponseEntity.ok().build();
+
+	@Autowired
+	private ServiceBot botService;
+
+	@GetMapping("/bot/user/{id}")
+	public ResponseEntity<Object> loadAvailableBots(@PathVariable Long id) {
+		List<Bot> bots = botService.findAll();
+		logger.info("Carregando [{}] bots disponiveis para o usuario [{}]", bots.size(), id);
+		return ResponseEntity.ok(bots);
 	}
 
 }
