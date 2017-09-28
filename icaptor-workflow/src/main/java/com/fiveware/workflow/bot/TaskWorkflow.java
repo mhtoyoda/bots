f958c15dc960d74c51b2fe13db988cb1e94a9327
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-09-28 16:34:17 -0300

    Ajuste de tempo para sequencia workflow

--- icaptor-workflow/src/main/java/com/fiveware/workflow/bot/TaskWorkflow.java
+++ icaptor-workflow/src/main/java/com/fiveware/workflow/bot/TaskWorkflow.java
@@ -1,5 +1,7 @@
 package com.fiveware.workflow.bot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class TaskWorkflow {
 
+	private static final Logger log = LoggerFactory.getLogger(TaskWorkflow.class);
+	
 	@Autowired
 	private SequenceTaskWorkflow sequenceTaskWorkflow;
 	
@@ -15,11 +19,13 @@ public class TaskWorkflow {
 	
 	@Scheduled(cron = "*/60 * * * * *")
 	public void createWorkFlowTaskCron(){
+		log.info("Execute createWorkFlowTaskCron");
 		createWorkflowTask.createWorkflows();
 	}
 
-	@Scheduled(cron = "0 0/5 * * * ?")
+	@Scheduled(cron = "0 0/3 * * * ?")
 	public void sequenceTask(){
+		log.info("Execute sequenceTask");
 		sequenceTaskWorkflow.sequenceTask();
 	}
 }
