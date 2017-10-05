7fa925d262dfbdc1918c9931a2ef1ed551104d86
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-10-05 16:13:54 -0300

    Refactoring parametros da plataforma

--- icaptor-framework/src/main/java/com/fiveware/parameter/ParameterValue.java
+++ icaptor-framework/src/main/java/com/fiveware/parameter/ParameterValue.java
@@ -1,35 +1,34 @@
 package com.fiveware.parameter;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+import com.google.common.collect.Lists;
 
 public class ParameterValue implements Serializable {
 
-	private String field;
-	private String value;
+	private List<ParameterIcaptor> parameterList;
 
 	public ParameterValue() {
+		parameterList = Lists.newArrayList();
+	}
+	
+	public void add(String type, String field, String value) {
+		ParameterIcaptor icaptorParameter = new ParameterIcaptor(type, field, value);
+		parameterList.add(icaptorParameter);
 	}
 
-	public ParameterValue(String field, String value) {
-		super();
-		this.field = field;
-		this.value = value;
+	public ParameterIcaptor getByType(String type){
+		Optional<ParameterIcaptor> optional = parameterList.stream().filter(p -> p.getType().equals(type)).findFirst();
+		return optional.get();
 	}
 
-	public String getField() {
-		return field;
+	public List<ParameterIcaptor> getParameterList() {
+		return parameterList;
 	}
 
-	public void setField(String field) {
-		this.field = field;
+	public void setParameterList(List<ParameterIcaptor> parameterList) {
+		this.parameterList = parameterList;
 	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-
 }
