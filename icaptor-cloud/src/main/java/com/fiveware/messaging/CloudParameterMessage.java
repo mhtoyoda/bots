3b915a39d7fc83e75e8c6430c6e2c11989229092
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-10-05 16:13:54 -0300

    Refactoring parametros da plataforma

--- icaptor-cloud/src/main/java/com/fiveware/messaging/CloudParameterMessage.java
+++ icaptor-cloud/src/main/java/com/fiveware/messaging/CloudParameterMessage.java
@@ -1,5 +1,8 @@
 package com.fiveware.messaging;
 
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +12,9 @@ import org.springframework.stereotype.Component;
 import com.fiveware.model.Parameter;
 import com.fiveware.model.message.MessageAgent;
 import com.fiveware.model.message.MessageParameterAgent;
+import com.fiveware.parameter.ParameterInfo;
 import com.fiveware.parameter.ParameterResolver;
+import com.google.common.collect.Lists;
 
 @Component("PARAMETERS")
 public class CloudParameterMessage implements ConsumerTypeMessage<MessageParameterAgent> {
@@ -26,10 +31,21 @@ public class CloudParameterMessage implements ConsumerTypeMessage<MessageParamet
 	@Override
 	public void process(MessageParameterAgent message) {
 		logger.info("Request Parameters Agent: {}", message.getAgent());
+		List<String> list = Lists.newArrayList();
 		Parameter parameterCredential = parameterResolver.getParameterCredential(message.getAgent(), message.getNameBot());
 		if(null != parameterCredential){
-			message.setFieldValue(parameterCredential.getFieldValue());			
+			list.add("credential:"+parameterCredential.getTypeParameter().getName()+":"+parameterCredential.getFieldValue());			
 		}
+		
+		ParameterInfo parameterInfo = parameterResolver.getParameterByBot(message.getNameBot());
+		Map<String, Parameter> parameters = parameterInfo.getParameters();
+		if(null != parameters){
+			for(Map.Entry<String, Parameter> parameter : parameters.entrySet()){			 
+				String param = "parameter:"+parameter.getKey()+":"+parameter.getValue().getFieldValue();			 
+				list.add(param);
+			}
+		}
+		message.setFieldValue(list);
 		producer.send("parameter."+message.getAgent(), message);
 	}
 }
---
+++ icaptor-framework/src/main/java/com/fiveware/parameter/ParameterIcaptor.java
@@ -0,0 +1,37 @@
+package com.fiveware.parameter;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ParameterIcaptor implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2590382722023093609L;
+	private String type;
+	private String field;
+	private String value;
+
+	@JsonCreator
+	public ParameterIcaptor(@JsonProperty("type") String type, @JsonProperty("field") String field, @JsonProperty("value") String value) {
+		this.type = type;
+		this.field = field;
+		this.value = value;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+}
