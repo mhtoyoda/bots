4cd0b2dbe952c279d128e818def92c458ebdc75b
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-10-16 00:10:19 -0200

    Alteracao de split json workflow

--- icaptor-workflow/src/main/java/com/fiveware/workflow/bot/ProcessWorkflowTask.java
+++ icaptor-workflow/src/main/java/com/fiveware/workflow/bot/ProcessWorkflowTask.java
@@ -14,6 +14,7 @@ import com.fiveware.task.TaskManager;
 import com.fiveware.workflow.exception.TaskCreateException;
 import com.fiveware.workflow.model.WorkflowBot;
 import com.fiveware.workflow.model.WorkflowBotStep;
+import com.fiveware.workflow.parse.SplitJson;
 import com.google.common.collect.Lists;
 
 @Component
@@ -21,6 +22,9 @@ public class ProcessWorkflowTask implements WorkflowSequence {
 	
 	@Autowired
 	private TaskManager taskManager;
+	
+	@Autowired
+	private SplitJson splitJson;
 	
 	@Override
 	public Long createTask(WorkflowBotStep workflowBotStep) throws TaskCreateException {
@@ -35,17 +39,37 @@ public class ProcessWorkflowTask implements WorkflowSequence {
 	}
 	
 	@Override
-	public Long createTaskByWorkflow(WorkflowBot workflowBot) {		
-		Task createTask = null;
+	public Long createTaskByWorkflow(WorkflowBot workflowBot) {
 		List<ItemTask> list = taskManager.itemTaskListStatus(Lists.newArrayList(StatusProcessItemTaskEnum.SUCCESS.getName()), workflowBot.getTaskId());
 		if(CollectionUtils.isNotEmpty(list)){
-			createTask = taskManager.createTask(workflowBot.getWorkflowBotStep().getBotTarget(), 1L);			
-			for (ItemTask itemTask : list) {									
-				taskManager.createItemTask(createTask, itemTask.getDataOut());			
-			}
-			createTask = taskManager.updateTask(createTask.getId(), StatusProcessTaskEnum.PROCESSING);
-			return createTask.getId();
+			return createItemTask(workflowBot, list);
 		}
 		return null;
+	}
+
+	private Long createTaskWorkflowByitemTask(WorkflowBot workflowBot, List<ItemTask> list) {
+		Task createTask = taskManager.createTask(workflowBot.getWorkflowBotStep().getBotTarget(), 1L);			
+		for (ItemTask itemTask : list) {									
+			taskManager.createItemTask(createTask, itemTask.getDataOut());			
+		}
+		createTask = taskManager.updateTask(createTask.getId(), StatusProcessTaskEnum.PROCESSING);
+		return createTask.getId();
+	}
+	
+	private Long createItemTask(WorkflowBot workflowBot, List<ItemTask> list){
+		if(null != workflowBot.getWorkflowBotStep() && workflowBot.getWorkflowBotStep().getBotTarget().equals("votorantimRC")){
+			Task task = taskManager.createTask(workflowBot.getWorkflowBotStep().getBotTarget(), 1L);
+			for (ItemTask itemTask : list) {
+				List<String> arrayJson = splitJson.splitArray(itemTask.getDataOut());
+				for (String json : arrayJson) {
+					System.out.println("JSON "+json);
+					taskManager.createItemTask(task, json);
+				}
+			}
+			task = taskManager.updateTask(task.getId(), StatusProcessTaskEnum.PROCESSING);
+			return task.getId();
+		}else{
+			return createTaskWorkflowByitemTask(workflowBot, list);
+		}
 	}
 }
