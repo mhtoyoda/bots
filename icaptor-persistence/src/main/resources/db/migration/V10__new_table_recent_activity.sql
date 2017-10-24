607b35ddda13db75dde9d3a5b0ebdd7aec86b79b
diego.souza <diego.souza@fiveware.com.br>
2017-10-24 12:09:07 -0200

    Alteracoes na tabela foram colocadas no script de criacao da tabela.

--- icaptor-persistence/src/main/resources/db/migration/V10__new_table_recent_activity.sql
+++ icaptor-persistence/src/main/resources/db/migration/V10__new_table_recent_activity.sql
@@ -3,6 +3,11 @@ CREATE TABLE recent_activity (
     message VARCHAR(50) NOT NULL,
 	creation_time datetime NOT NULL,
     user_id BIGINT(20) NOT NULL,
+    task_id BIGINT(20) NULL,
+    bot_id BIGINT(20) NULL,
+    visualized BOOLEAN DEFAULT false,
 
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (task_id) REFERENCES task(id),
+    FOREIGN KEY (bot_id) REFERENCES bot(id)
 )ENGINE=InnoDB DEFAULT charset=utf8;
