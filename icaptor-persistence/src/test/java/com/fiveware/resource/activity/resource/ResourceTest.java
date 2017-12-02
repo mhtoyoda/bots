bf94fc8ef80730bee9167147cc292588b2e3ccf4
Valdisnei <valdisnei@MacBook-Pro-de-Valdisnei.local>
2017-12-02 15:30:40 -0200

    #ICAPTOR-247- modificado classe para testar implementacao de IT

--- icaptor-persistence/src/test/java/com/fiveware/resource/activity/resource/ResourceTest.java
+++ icaptor-persistence/src/test/java/com/fiveware/resource/activity/resource/ResourceTest.java
@@ -14,15 +14,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.IfProfileValue;
-import org.springframework.test.annotation.ProfileValueSourceConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.core.env.AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
