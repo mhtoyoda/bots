83251a5439118f9375ca291b4f37308c8e648611
Valdisnei <valdisnei@MacBook-Pro-de-Valdisnei.local>
2017-12-03 09:59:27 -0200

    #ICAPTOR-247- modificado classe para testar implementacao de IT

--- icaptor-service-rest/src/main/java/com/fiveware/model/activity/RecentActivity.java
+++ icaptor-service-rest/src/main/java/com/fiveware/model/activity/RecentActivity.java
@@ -23,7 +23,7 @@ public class RecentActivity {
 	@Column(name = "creation_time")
 	private LocalDateTime creationTime;
 
-	@ManyToOne(cascade = CascadeType.ALL)
+	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private IcaptorUser user;
 	
