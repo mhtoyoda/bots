a2b76810647499f33084f8cac2ea71dee5cb9a63
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-12-06 14:16:56 -0200

    Teste agentConfig

---
+++ icaptor-agent/src/test/java/com/fiveware/config/agent/AgentConfigTest.java
@@ -0,0 +1,69 @@
+package com.fiveware.config.agent;
+
+import com.fiveware.loader.ClassLoaderConfig;
+import com.fiveware.messaging.BrokerManager;
+import com.fiveware.messaging.Producer;
+import com.fiveware.model.message.MessageAgent;
+import com.google.common.collect.Lists;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.verifyPrivate;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(AgentConfig.class)
+public class AgentConfigTest {
+
+    @Mock
+    private AgentConfigProperties data;
+
+    @Mock
+    private ClassLoaderConfig classLoaderConfig;
+
+    @Mock
+    private AgentListener agentListener;
+
+    @Mock
+    private BrokerManager brokerManager;
+
+    @Mock
+    private Producer<MessageAgent> producer;
+
+    @InjectMocks
+    private AgentConfig agentConfig;
+
+    private AgentConfig agentConfigSpy;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+        agentConfigSpy = PowerMockito.spy(agentConfig);
+    }
+
+    @Test
+    public void initAgentTest() throws Exception {
+        when(agentListener.getAgentPort()).thenReturn(8080);
+        agentConfigSpy.initAgent();
+        verifyPrivate(agentConfigSpy).invoke("bindQueueAgenteInTaskTopic", "topic-exchange", Lists.newArrayList(data.getAgentName()));
+        verifyPrivate(agentConfigSpy).invoke("createQueueParameter");
+        verifyPrivate(agentConfigSpy).invoke("notifyServerUpAgent");
+    }
+
+    @Test
+    public void initAgentPortZeroTest() throws Exception {
+        when(agentListener.getAgentPort()).thenReturn(0);
+        agentConfigSpy.initAgent();
+        verifyZeroInteractions(brokerManager);
+        verifyZeroInteractions(producer);
+    }
+}
